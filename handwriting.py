41218c89da8802de44e7e738532963763fe33745
luoyif <932082545@qq.com>
2024-04-16 23:20:53 +0800

    new

---
+++ handwriting.py
@@ -0,0 +1,50 @@
+import sys
+import tensorflow as tf
+
+# Use MNIST handwriting dataset
+mnist = tf.keras.datasets.mnist
+
+# Prepare data for training
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+y_train = tf.keras.utils.to_categorical(y_train)
+y_test = tf.keras.utils.to_categorical(y_test)
+x_train = x_train.reshape(
+    x_train.shape[0], x_train.shape[1], x_train.shape[2], 1
+)
+x_test = x_test.reshape(
+    x_test.shape[0], x_test.shape[1], x_test.shape[2], 1
+)
+
+# Create a convolutional neural network
+model = tf.keras.models.Sequential([
+
+    #1.  Convolutional layer. Learn 32 filters using a 3x3 kernel, activation function is relu, input shape (28,28,1)
+
+
+    #2. Max-pooling layer, using 2x2 pool size
+
+    #3.  Flatten units
+
+    #4. Add a hidden layer with dropout, 
+
+    #5. Add an output layer with output units for all 10 digits, activation function is softmax
+    
+])
+
+# Train neural network
+model.compile(
+    optimizer="adam",
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+)
+model.fit(x_train, y_train, epochs=10)
+
+# Evaluate neural network performance
+model.evaluate(x_test,  y_test, verbose=2)
+
+# Save model to file
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+    model.save(filename)
+    print(f"Model saved to {filename}.")
